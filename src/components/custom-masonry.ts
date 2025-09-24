5e5b8d132c136185559bd5ec06e28fdefb3817b2
ndoherty <ndoherty.xyz@gmail.com>
2025-09-24 12:34:31 -0600

    dockerfile fix attempt

--- src/components/custom-masonry.ts
+++ src/components/custom-masonry.ts
@@ -45,6 +45,7 @@ export function CustomMasonry<Item>(
   );
 
   const maybeLoadMore = useInfiniteLoader(fetchMoreItems, {
+    isItemLoaded: (index, items) => !!items[index],
     ...(props.fetchMoreThreshold
       ? { threshold: props.fetchMoreThreshold }
       : {}),
